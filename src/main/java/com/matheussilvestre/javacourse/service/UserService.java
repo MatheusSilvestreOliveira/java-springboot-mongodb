d9e113e78c90951f0f7aa9b563096f248a03401f
Matheus Silvestre <mathsylvester13@gmail.com>
2022-04-27 14:57:54 -0300

    Obtendo um usuario por id

--- src/main/java/com/matheussilvestre/javacourse/service/UserService.java
+++ src/main/java/com/matheussilvestre/javacourse/service/UserService.java
@@ -1,12 +1,14 @@
 package com.matheussilvestre.javacourse.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheussilvestre.javacourse.domain.User;
 import com.matheussilvestre.javacourse.repository.UserRepository;
+import com.matheussilvestre.javacourse.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 	
 }
---
+++ src/main/java/com/matheussilvestre/javacourse/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.matheussilvestre.javacourse.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
