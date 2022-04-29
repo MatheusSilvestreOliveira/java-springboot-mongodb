2f584edd979a3cefbc0544ba6112c7039d92fa9c
Matheus Silvestre <mathsylvester13@gmail.com>
2022-04-29 11:02:36 -0300

    POST para inserir usuario

--- src/main/java/com/matheussilvestre/javacourse/service/UserService.java
+++ src/main/java/com/matheussilvestre/javacourse/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheussilvestre.javacourse.domain.User;
+import com.matheussilvestre.javacourse.dto.UserDTO;
 import com.matheussilvestre.javacourse.repository.UserRepository;
 import com.matheussilvestre.javacourse.service.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
