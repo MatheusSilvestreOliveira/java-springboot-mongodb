f12a87ddd89b4d537fa0d3239e7b3f00bc212bbb
Matheus Silvestre <mathsylvester13@gmail.com>
2022-05-02 11:49:33 -0300

    GET posts by id

---
+++ src/main/java/com/matheussilvestre/javacourse/service/PostService.java
@@ -0,0 +1,25 @@
+package com.matheussilvestre.javacourse.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheussilvestre.javacourse.domain.Post;
+import com.matheussilvestre.javacourse.repository.PostRepository;
+import com.matheussilvestre.javacourse.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	
+	
+}
