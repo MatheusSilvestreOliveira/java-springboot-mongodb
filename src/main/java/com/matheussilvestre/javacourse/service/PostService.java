45e0968eb56b180b523fe79d23df459eb5214f8c
Matheus Silvestre <mathsylvester13@gmail.com>
2022-05-03 11:20:07 -0300

    Consulta @Query com varios criterios

--- src/main/java/com/matheussilvestre/javacourse/service/PostService.java
+++ src/main/java/com/matheussilvestre/javacourse/service/PostService.java
@@ -1,5 +1,6 @@
 package com.matheussilvestre.javacourse.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,8 +24,14 @@ public class PostService {
 	
 	
 	public List<Post> findByTitle(String txt){
+		//return repo.findByTitleContainingIgnoreCase(txt);
 		return repo.searchTitle(txt);
 	}
 	
+	
+	public List<Post> fullSearch(String text, Date minDat, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDat, maxDate);
+	}
 	
 }
